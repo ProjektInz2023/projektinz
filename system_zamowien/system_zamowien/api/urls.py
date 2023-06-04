17ad0868428a797dfa225def249ff029fd0c7df9
Kamil Berenhard <kamilberenhard@wp.pl>
2023-06-04 18:11:13 +0200

    adding httponly token

--- system_zamowien/system_zamowien/api/urls.py
+++ system_zamowien/system_zamowien/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from api.views import MyTokenObtainPairView
+from .views import LoginView
 
 
 urlpatterns = [
-    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(), name="login"),
 ]
