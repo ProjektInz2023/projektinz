3417ea2a7a0db702ba2aee1b9587c44170d516f9
Kacper <kacpercichy@onet.pl>
2024-01-03 18:28:41 +0100

    small changes 2

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -30,6 +30,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AdminLoginView.vue')
+  },
+  {
+    path: '/admin-account',
+    name: 'Admin-Account',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminAccountView.vue')
   }
 ]
 
