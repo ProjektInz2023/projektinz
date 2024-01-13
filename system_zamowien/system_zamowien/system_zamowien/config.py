86f0e45a8a47b900349638a7fbb54f940d426b50
HerrKurz <aleksander.kurz@gmail.com>
2024-01-13 21:38:45 +0100

    remove type force

--- system_zamowien/system_zamowien/system_zamowien/config.py
+++ system_zamowien/system_zamowien/system_zamowien/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Database connector variables
 DB_URL = os.getenv("DB_URL")
-DB_PORT = int(os.getenv("DB_PORT"))
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
