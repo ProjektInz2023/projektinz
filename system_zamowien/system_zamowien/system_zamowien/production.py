7f2b3cd21315987063d5e056d75a3aeffe71035f
HerrKurz <aleksander.kurz@gmail.com>
2024-01-14 01:30:54 +0100

    update wsgi and production deployment files

---
+++ system_zamowien/system_zamowien/system_zamowien/production.py
@@ -0,0 +1,36 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+DEBUG = False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+from .config import DB_NAME, DB_PASSWORD, DB_URL, DB_USER, DB_PORT
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_URL,
+        'PORT': DB_PORT,
+    }
+}
--- system_zamowien/system_zamowien/system_zamowien/wsgi.py
+++ system_zamowien/system_zamowien/system_zamowien/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'system_zamowien.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'system_zamowien.settings'
-
+settings_module = 'system_zamowien.production' if 'WEBSITE_HOSTNAME' in os.environ else 'system_zamowien.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
+
+
